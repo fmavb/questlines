d254247b84150253a90a28619c8ff4a34c1d5c6e
Alexander Behrmann <alexander.behrmann@hotmail.com>
2020-01-02 11:17:52 +0100

    challenge 3 start

--- src/components/Challenge3.js
+++ src/components/Challenge3.js
@@ -1,14 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import FileBrowser, { FileRenderers, FolderRenderers, Groupers, Icons } from 'react-keyed-file-browser';
+import '../../node_modules/react-keyed-file-browser/dist/react-keyed-file-browser.css';
+import "./Challenge3.css";
 
-export default class Challenge3 extends Component{
-    constructor(props){
+export default class Challenge3 extends Component {
+    constructor(props) {
         super(props);
+        this.selectFile = this.selectFile.bind(this);
+        this.state = {files: [
+            {
+                key: 'patient1/record_encrypted',
+                size: 1.5 * 1024 * 1024,
+            },
+            {
+                key: 'patient1/record.txt',
+                size: 1.5 * 1024 * 1024
+            }
+        ]};
     }
-
-    render(){
+    selectFile(e){
+        console.log(e);
+    }
+    render() {
         return (
-            <div>
-                <h1>Work in Progress</h1>
+            <div className="page">
+                <div className="browser">
+                <FileBrowser
+                    icons={Icons.FontAwesome(4)}
+                    files={this.state.files}
+                    onSelectFile={this.selectFile}
+                />
+                </div>
+                <div className="dropzone">
+
+                </div>
             </div>
         );
     }
